e30a060ff37edf3bb1d849caf3565492ba76d4c8
Ryan Wang <wanglun02@gmail.com>
2016-05-27 19:20:32 +0800

    更新JS

--- Pod/Assets/EMJSBridge.js
+++ Pod/Assets/EMJSBridge.js
@@ -1,10 +1,10 @@
-(function() {
+(function () {
   window.AppURLScheme = "emstock";
 
   function ParseParam(obj) {
     var params = [];
     for (var p in obj) {
-      if (typeof(obj[p]) == "undefined" || obj[p] == "undefined") {
+      if (typeof (obj[p]) == "undefined" || obj[p] == "undefined") {
         obj[p] = "";
       }
       params.push(p + "=" + encodeURIComponent(obj[p]));
@@ -30,23 +30,23 @@
     doc.documentElement.appendChild(messagingIframe);
     doc.documentElement.removeChild(messagingIframe);
   }
- 
- function prepareWebViewJavascriptBridge(callback) {
-      if (window.WebViewJavascriptBridge) {
-        return callback(WebViewJavascriptBridge);
-      }
-      if (window.WVJBCallbacks) {
-        return window.WVJBCallbacks.push(callback);
-      }
-      window.WVJBCallbacks = [callback];
-      var WVJBIframe = document.createElement('iframe');
-      WVJBIframe.style.display = 'none';
-      WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
-      document.documentElement.appendChild(WVJBIframe);
-      setTimeout(function() {
-        document.documentElement.removeChild(WVJBIframe)
-      }, 0)
- }
+
+  function prepareWebViewJavascriptBridge(callback) {
+    if (window.WebViewJavascriptBridge) {
+      return callback(WebViewJavascriptBridge);
+    }
+    if (window.WVJBCallbacks) {
+      return window.WVJBCallbacks.push(callback);
+    }
+    window.WVJBCallbacks = [callback];
+    var WVJBIframe = document.createElement('iframe');
+    WVJBIframe.style.display = 'none';
+    WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+    document.documentElement.appendChild(WVJBIframe);
+    setTimeout(function () {
+      document.documentElement.removeChild(WVJBIframe)
+    }, 0)
+  }
 
 
   if (window.goods) {
@@ -55,22 +55,22 @@
 
   // 如果不支持WebViewJavascriptBridge 则使用老的方式
   window.GoodsBridge = {
-    callHandler: function(handlerName, data, responseCallback) {
-      prepareWebViewJavascriptBridge(function (){});
- 
+    callHandler: function (handlerName, data, responseCallback) {
+      prepareWebViewJavascriptBridge(function () { });
+
       if (window.WebViewJavascriptBridge) {
         var parameters = data;
-        if (typeof(data) === "string") {
+        if (typeof (data) === "string") {
           parameters = JSON.parse(data);
         }
-        
-        if(responseCallback) {
+
+        if (responseCallback) {
           window.WebViewJavascriptBridge.callHandler(handlerName, parameters, responseCallback);
         } else {
-          window.WebViewJavascriptBridge.callHandler(handlerName, parameters, function(result){
-            var callback = parameters["callback"]; 
+          window.WebViewJavascriptBridge.callHandler(handlerName, parameters, function (result) {
+            var callback = parameters["callback"];
             if (callback) {
-              eval(callback)(result);  
+              eval(callback)(result);
             }
           });
         }
@@ -81,61 +81,61 @@
   };
 
   window.goods = {
-    ready: function(callback) {
+    ready: function (callback) {
       prepareWebViewJavascriptBridge(callback);
     },
 
-    goback: function() {
+    goback: function () {
       GoodsBridge.callHandler("back");
     },
 
-    openurl: function(url) {
+    openurl: function (url) {
       var params = {
         "url": url
       };
-      GoodsBridge.callHandler('web', params, function(response) {})
+      GoodsBridge.callHandler('web', params, function (response) { })
     },
 
-    close: function() {
-      GoodsBridge.callHandler('close', null, function(response) {})
+    close: function () {
+      GoodsBridge.callHandler('close', null, function (response) { })
     },
 
-    copy: function(text) {
+    copy: function (text) {
       var params = {
         "text": text
       };
-      GoodsBridge.callHandler('copy', params, function(response) {})
+      GoodsBridge.callHandler('copy', params, function (response) { })
     },
 
-    openPage: function(pageId) {
+    openPage: function (pageId) {
       var params = {
         "pageId": pageId
       };
-      GoodsBridge.callHandler("page", params, function(
-        response) {})
+      GoodsBridge.callHandler("page", params, function (
+        response) { })
     },
 
     // 2.9.0
     // {pageId:104000, stockCode:2000,stockName:10000}
-    routePage: function(params) {
-      GoodsBridge.callHandler("routePage", params, function(
-        response) {})
+    routePage: function (params) {
+      GoodsBridge.callHandler("routePage", params, function (
+        response) { })
     },
 
     // 2.9.0
-    route: function(path, params) {
+    route: function (path, params) {
       params.path = path;
-      GoodsBridge.callHandler("route", params, function(
-        response) {})
+      GoodsBridge.callHandler("route", params, function (
+        response) { })
     },
-    
-    showMenuItems:function(params) {
-      GoodsBridge.callHandler("showMenuItems", params, function(
-        response) {})
+
+    showMenuItems: function (params) {
+      GoodsBridge.callHandler("showMenuItems", params, function (
+        response) { })
     },
 
     // 页面跳转类型
-    showgoods: function(stockId, fk, goodsName, subType) {
+    showgoods: function (stockId, fk, goodsName, subType) {
       var params = {
         "stockId": stockId,
         "subType": subType,
@@ -146,17 +146,17 @@
     },
 
     // 自选股模块
-    addZxg: function(stockId, callback) {
+    addZxg: function (stockId, callback) {
       var params = {
         "stockId": stockId,
         "callback": callback
       };
-      GoodsBridge.callHandler('addZXG', params, function(
-        response) {})
+      GoodsBridge.callHandler('addZXG', params, function (
+        response) { })
     },
 
     // 授权模块
-    login: function(gowhere, callback) {
+    login: function (gowhere, callback) {
       var params = {
         "next": gowhere,
         "callback": callback
@@ -165,24 +165,24 @@
     },
 
     // Not implement
-    purchase: function() {},
+    purchase: function () { },
 
     // 搜索模块
-    search: function(searchStr, type, callback) {
+    search: function (searchStr, type, callback) {
       var params = {
         "content": searchStr,
         "type": type,
         "callback": callback
       };
-      GoodsBridge.callHandler('search', params, function(response) {})
+      GoodsBridge.callHandler('search', params, function (response) { })
     },
 
-    homepage: function() {
+    homepage: function () {
       goods.route('home', params);
     },
 
     // 分享模块
-    share: function(title, url, id, imageurl, iconUrl, content, type,
+    share: function (title, url, id, imageurl, iconUrl, content, type,
       callback) {
       var params = {
         "title": title,
@@ -194,32 +194,32 @@
         "type": type,
         "callback": callback
       };
-      GoodsBridge.callHandler('share', params, function(response) {})
+      GoodsBridge.callHandler('share', params, function (response) { })
     },
 
     // 任务模块
-    completeTask: function(taskId, callback) {
+    completeTask: function (taskId, callback) {
       var params = {
         "taskId": taskId,
         "callback": callback
       };
-      GoodsBridge.callHandler('completeTask', params, function(
-        response) {})
+      GoodsBridge.callHandler('completeTask', params, function (
+        response) { })
 
     },
 
-    checkTaskStatus: function(operationId, callback) {
+    checkTaskStatus: function (operationId, callback) {
       var params = {
         "operationId": operationId,
         "callback": callback
       };
 
-      GoodsBridge.callHandler('checkTaskStatus', params, function(
-        response) {})
+      GoodsBridge.callHandler('checkTaskStatus', params, function (
+        response) { })
     },
 
     // 社区发帖
-    writePost: function(barId, barType, topicType, wordslimit, callback) {
+    writePost: function (barId, barType, topicType, wordslimit, callback) {
       var params = {
         "barId": barId,
         "barType": barType,
@@ -230,7 +230,7 @@
       goods.route("writePost", params);
     },
 
-    sendPost: function(barId, barType, topicType, title, content,
+    sendPost: function (barId, barType, topicType, title, content,
       wordslimit,
       callback) {
       var params = {
@@ -245,7 +245,7 @@
       goods.route("sendPost", params);
     },
 
-    reply: function(topidid, quotoid, wordslimit, replyTo, callback) {
+    reply: function (topidid, quotoid, wordslimit, replyTo, callback) {
       var params = {
         "topicId": topidid,
         "quotoId": quotoid,
@@ -256,7 +256,7 @@
       goods.route("reply", params);
     },
 
-    replyHalfScreen: function(topicId, postId, wordLimit, replyTo, callback) {
+    replyHalfScreen: function (topicId, postId, wordLimit, replyTo, callback) {
       var params = {
         "topicId": topicId,
         "postId": postId,
@@ -267,14 +267,14 @@
       goods.route("replyHalfScreen", params);
     },
 
-    myFunsTopics: function(userId) {
+    myFunsTopics: function (userId) {
       var params = {
         "userId": userId
       };
       goods.route("friendList", params);
     },
 
-    openCommentList: function(url, topicId) {
+    openCommentList: function (url, topicId) {
       var params = {
         "url": url,
         "topicId": topicId
@@ -283,7 +283,7 @@
     },
 
     // 用户信息模块
-    pointChange: function(point, pointChange, integral, showNotify) {
+    pointChange: function (point, pointChange, integral, showNotify) {
       var params = {
         "point": point,
         "pointChange": pointChange,
@@ -291,12 +291,12 @@
         "showNotify": showNotify
       };
 
-      GoodsBridge.callHandler('pointsChange', params, function(
-        response) {})
+      GoodsBridge.callHandler('pointsChange', params, function (
+        response) { })
 
     },
 
-    updateUserInfo: function(exp, point, level, nextlvexp, bitmapNewapp) {
+    updateUserInfo: function (exp, point, level, nextlvexp, bitmapNewapp) {
       var params = {
         "exp": exp,
         "point": point,
@@ -304,12 +304,12 @@
         "nextlvexp": nextlvexp,
         "bitmapNewapp": bitmapNewapp
       };
-      GoodsBridge.callHandler('updateUserInfo', params, function(
-        response) {})
+      GoodsBridge.callHandler('updateUserInfo', params, function (
+        response) { })
     },
 
     // 视频播放
-    playVideo: function(id, sourceType, url, domain, meetingId, title,
+    playVideo: function (id, sourceType, url, domain, meetingId, title,
       videoStatus) {
       var params = {
         "id": id,
@@ -324,7 +324,7 @@
     },
 
 
-    openAccount: function() {
+    openAccount: function () {
       goods.route("openAccount", params);
     },
 
@@ -332,25 +332,25 @@
     // 理财
     // 页面跳转类型的使用openPath
     // 获取数据的使用GoodsBridge
-    getFundAccount: function(vendorId) {
+    getFundAccount: function (vendorId) {
       var params = {
         "vendorId": vendorId
       };
       GoodsBridge.callHandler('getFundAccount', params, null);
     },
 
-    goFundMyAsset: function() {
+    goFundMyAsset: function () {
       GoodsBridge.callHandler('goFundMyAsset', null, null);
     },
 
-    showNotify: function(message) {
+    showNotify: function (message) {
       var params = {
         "message": message
       };
       GoodsBridge.callHandler('showNotify', params, null);
     },
 
-    shareConfig: function(shareToggle, title, url, imageurl, content) {
+    shareConfig: function (shareToggle, title, url, imageurl, content) {
       var params = {
         "shareToggle": shareToggle,
         "title": title,
@@ -358,70 +358,73 @@
         "imageurl": imageurl,
         "content": content
       };
-      GoodsBridge.callHandler('shareConfig', params, function(
-        response) {})
+      GoodsBridge.callHandler('shareConfig', params, function (
+        response) { })
     },
 
-    searchConfig: function(searchToggle) {
+    searchConfig: function (searchToggle) {
       var params = {
         "searchToggle": searchToggle,
       };
-      GoodsBridge.callHandler('searchConfig', params, function(
-        response) {})
+      GoodsBridge.callHandler('searchConfig', params, function (
+        response) { })
     },
 
     // 2.8.4
-    heightChange: function(webViewheight, type) {
+    heightChange: function (webViewheight, type) {
       var params = {
         "webViewheight": webViewheight,
         "type": type,
       };
- 
-      GoodsBridge.callHandler('heightChange', params, function(
-        response) {})
+
+      GoodsBridge.callHandler('heightChange', params, function (
+        response) { })
     },
 
     // 2.9.0
     // @params: {appurl:"emstock://"}
-    canOpenURL2: function(params, responseCallback) {
+    canOpenURL2: function (params, responseCallback) {
       GoodsBridge.callHandler('canOpenURL2', params, responseCallback)
     },
 
     // 2.9.0+ 改成回调方式
-    getAppInfo2: function(params, responseCallback) {
+    getAppInfo2: function (params, responseCallback) {
       GoodsBridge.callHandler('getAppInfo2', params,
         responseCallback);
     },
 
     // 2.9.0+
-    updateTitle: function(params, responseCallback) {
+    updateTitle: function (params, responseCallback) {
       GoodsBridge.callHandler('updateTitle', params,
         responseCallback);
     },
 
     // 2.9.0+
-    post: function(params, responseCallback) {
+    post: function (params, responseCallback) {
       GoodsBridge.callHandler('post', params,
         responseCallback);
     },
 
-    installPlugin: function(plugin) {
+    installPlugin: function (plugin) {
       for (var item in plugin) {
         goods[item] = plugin[item];
       }
     }
   };
 
-}());
+} ());
 
-(function() {
+(function () {
   var ev = document.createEvent("Event");
   ev.initEvent("goodsReady", true, true);
   document.dispatchEvent(ev);
-}());
+} ());
 
-//炒盘技巧--区分是否有此任务
-if (typeof(clientGoods) != "undefined" && clientGoods != null && typeof(
+document.addEventListener("DOMContentLoaded", function () {
+  //炒盘技巧--区分是否有此任务
+  if (typeof (clientGoods) != "undefined" && clientGoods != null && typeof (
     clientGoods.initTasks) != "undefined" && clientGoods.initTasks != null) {
-  clientGoods.initTasks();
-}
+    clientGoods.initTasks();
+  }
+
+})

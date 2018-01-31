1b5ee823adfff1c87786600b20f6c3adf1f3d4d9
Renford <chaofree3088@sina.com>
2018-01-31 13:15:06 +0800

    web url 参数去重

--- Pod/WebViewController/Class/JLRoutes+WebApp.m
+++ Pod/WebViewController/Class/JLRoutes+WebApp.m
@@ -21,34 +21,21 @@
 }
 
 - (void)registerWebWithAppSettings:(id<MSAppSettings>)settings {
+    __weak __typeof(self)weakSelf = self;
     [self addRoute:@"web" handler:^BOOL(NSDictionary<NSString *,id> * _Nonnull parameters) {
         NSURL *URL = [NSURL URLWithString:parameters[@"url"]];
         NSString *scheme = [URL scheme];
         NSString *lowerScheme = [scheme lowercaseString];
         if ([settings.supportsURLSchemes containsObject:scheme]) {
+
             return [JLRoutes routeURL:URL];
+
         } else if ([lowerScheme isEqualToString:@"http"] ||
                    [lowerScheme isEqualToString:@"https"] ||
                    [lowerScheme hasPrefix:@"file"]) {
 
-            // url adapte theme
-            NSMutableDictionary *tempDic = [NSMutableDictionary dictionaryWithDictionary:parameters];
-            if ([[MSThemeManager currentTheme] isEqualToString:@"black"]) {
-                NSString *urlString = parameters[@"url"];
-                if (![urlString containsString:@"css=night"]) {
-                    if ([urlString containsString:@"?"]) {
-                        urlString = [urlString stringByAppendingString:@"&"];
-                    } else {
-                        urlString = [urlString stringByAppendingString:@"?"];
-                    }
-                    urlString = [urlString stringByAppendingString:@"css=night"];
-                }
-                [tempDic setObject:urlString forKey:@"url"];
-            }
+            [weakSelf handleWebHttpRoute:parameters];
 
-            [MSActiveControllerFinder sharedFinder].resetStatus();
-            UINavigationController *navigator = [MSActiveControllerFinder sharedFinder].activeNavigationController();
-            [navigator pushViewControllerClass:NSClassFromString(@"EMWebViewController") params:tempDic];
             return true;
         } else {
             return false;
@@ -69,7 +56,7 @@
         }
         return YES;
     };
-    
+
     [self addRoutes:@[@"goBack", @"goback"] handler:completion];
 }
 
@@ -80,5 +67,45 @@
     [self removeRoute:@"goback"];
 }
 
+- (void)handleWebHttpRoute:(NSDictionary<NSString *, id> *)parameters
+{
+    NSMutableDictionary *tempDic = [NSMutableDictionary dictionaryWithDictionary:parameters];
+    NSString *urlString = parameters[@"url"];
+
+    urlString = [self handleUrl:urlString];
+    [tempDic setObject:urlString forKey:@"url"];
+
+    [MSActiveControllerFinder sharedFinder].resetStatus();
+    UINavigationController *navigator = [MSActiveControllerFinder sharedFinder].activeNavigationController();
+    [navigator pushViewControllerClass:NSClassFromString(@"EMWebViewController") params:tempDic];
+}
+
+/*
+ 处理url
+    1、去掉重复参数，过滤web的bug
+    2、处理黑白版url：黑板添加css=night，白板去掉css=night(如果拿到的 web url包含css=night)
+ */
+- (NSString *)handleUrl:(NSString *)urlString
+{
+    NSURLComponents *urlComponents = [[NSURLComponents alloc] initWithString:urlString];
+    NSMutableDictionary<NSString*, NSURLQueryItem*> *tempDic = [NSMutableDictionary dictionary];
+
+    for (NSURLQueryItem *item in urlComponents.queryItems) {
+        [tempDic setObject:item forKey:item.name];
+    }
+
+    if ([[MSThemeManager currentTheme] isEqualToString:@"black"]) {
+        NSURLQueryItem *item = [[NSURLQueryItem alloc] initWithName:@"css" value:@"night"];
+        [tempDic setObject:item forKey:item.name];
+    } else {
+        if ([tempDic.allKeys containsObject:@"css"]) {
+            [tempDic removeObjectForKey:@"css"];
+        }
+    }
+    urlComponents.queryItems = tempDic.allValues;
+
+    return urlComponents.URL.absoluteString;
+}
 
 @end
+
